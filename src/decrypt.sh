a2313ce975eed488c84efeed69195f52c56d4f45
Velizar Demirev <velizardemirev@gmail.com>
2024-07-19 15:25:26 +0200

    fix: use quotation marks for script

--- src/decrypt.sh
+++ src/decrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SECRET=$(echo -n $1 | openssl enc -aes-256-cbc -pbkdf2 -d -salt -k $2);
+SECRET=$(echo -n "$1" | openssl enc -aes-256-cbc -pbkdf2 -d -salt -k "$2");
 echo ::set-output name=decrypted-secret::$SECRET
