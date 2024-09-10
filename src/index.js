9043df627ac66c0f9a0fcdb527f19d36d7f93e6f
Velizar Demirev <velizardemirev@gmail.com>
2024-09-10 20:47:52 +0200

    feat: add initialization vector as input

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ async function run() {
     try{
         const secrets = core.getInput('secrets');
         const token = core.getInput('github-token');
+        const initializationVector = core.getInput('initialization-vector');
 
         const key = Buffer.from(token.slice(0, 32), 'utf8');
-        const iv = Buffer.from(token.slice(0, 16), 'utf8');
+        const iv = Buffer.from(initializationVector.slice(0, 16), 'utf8');
         const decipher = crypto.createDecipheriv('aes256', key, iv);
 
-        const decryptedSecrets = decipher.update(secrets, 'utf8', 'utf8');
+        const decryptedSecrets = decipher.update(secrets, 'utf8', 'hex');
         core.setOutput('decrypted-secret', decryptedSecrets);
     } catch (error) {
         core.setFailed(error.message);
